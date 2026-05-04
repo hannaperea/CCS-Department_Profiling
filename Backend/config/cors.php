318ce4bcb3e6f3cef37d2164cf24d651f9dd7f77
jarthurreyes30-tech <hannaperea110406@gmail.com>
2026-05-04 13:53:48 +0800

    new update
    updated

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -1,14 +1,16 @@
 <?php
 
 return [
-    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
+    'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout', '*'],
     'allowed_methods' => ['*'],
     'allowed_origins' => [
         'https://ccs-department-profiling.vercel.app',
-        'https://ccs-department-profiling-lss5447mq-hannapereas-projects.vercel.app',
         'http://localhost:5173',
     ],
-    'allowed_origins_patterns' => [],
+    'allowed_origins_patterns' => [
+        'https://.*-vercel\.app',
+        'https://ccs-department-profiling-.*\.vercel\.app',
+    ],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
     'max_age' => 0,
