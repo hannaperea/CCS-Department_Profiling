cf9a2308497c600d9d059d8274191495312bfae6
hannaperea <hannaperea110406@gmail.com>
2026-05-04 09:54:56 +0800

    Update cors.php

--- Backend/config/cors.php
+++ Backend/config/cors.php
@@ -1,31 +1,16 @@
 <?php
 
 return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cross-Origin Resource Sharing (CORS) Configuration
-    |--------------------------------------------------------------------------
-    */
-
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'logout'],
-
     'allowed_methods' => ['*'],
-
     'allowed_origins' => [
         'https://ccs-department-profiling.vercel.app',
         'https://ccs-department-profiling-lss5447mq-hannapereas-projects.vercel.app',
         'http://localhost:5173',
     ],
-
     'allowed_origins_patterns' => [],
-
     'allowed_headers' => ['*'],
-
     'exposed_headers' => [],
-
     'max_age' => 0,
-
     'supports_credentials' => true,
-
 ];
